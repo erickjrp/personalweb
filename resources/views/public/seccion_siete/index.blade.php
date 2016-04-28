42eb0460046a55de2223e308ea3ddb6d8391f49f
Erick Ramírez <eri_ramirez29@hotmail.com>
2016-04-28 14:50:11 -0430

    av

--- resources/views/public/seccion_siete/index.blade.php
+++ resources/views/public/seccion_siete/index.blade.php
@@ -4,9 +4,17 @@
 
 		<div class="col-xs-12 text-center">
 
-			<img src="{{ URL::asset('assets/images/seccion_siete/facebook.png') }}" class="inline-block" alt="Facebook">
-			<img src="{{ URL::asset('assets/images/seccion_siete/twitter.png') }}" class="inline-block" alt="Twitter">
-			<img src="{{ URL::asset('assets/images/seccion_siete/instagram.png') }}" class="inline-block" alt="Instagram">
+			<a href="https://www.facebook.com/erick.ramirez1991" target="_blank">
+				<img src="{{ URL::asset('assets/images/seccion_siete/facebook.png') }}" class="inline-block" alt="Facebook">
+			</a>
+
+			<a href="https://twitter.com/Erickjrp" target="_blank">
+				<img src="{{ URL::asset('assets/images/seccion_siete/twitter.png') }}" class="inline-block" alt="Twitter">
+			</a>
+
+			<a href="https://www.instagram.com/erickjrp/" target="_blank">
+				<img src="{{ URL::asset('assets/images/seccion_siete/instagram.png') }}" class="inline-block" alt="Instagram">
+			</a>
 
 		</div>
 
