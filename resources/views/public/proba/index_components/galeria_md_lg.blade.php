23b47ae4e417bc1e17e513016707317756af1248
Erick Ramírez <eri_ramirez29@hotmail.com>
2016-05-16 23:14:52 -0430

    avances

---
+++ resources/views/public/proba/index_components/galeria_md_lg.blade.php
@@ -0,0 +1,221 @@
+<div class="row hidden-xs hidden-sm">
+
+	<div class="col-md-5 col-lg-4">
+
+		<img class="center-block img-responsive" src="{{ URL::asset('assets/images/proba/index/teacher.png') }}">
+
+	</div>
+
+	<div class="col-md-7 col-lg-8">
+
+		<!--Carrusel-->
+
+		<div id="temas" class="carousel slide" data-ride="carousel" data-interval="false">
+			<!-- Indicators -->
+			<ol class="carousel-indicators carrusel-indicadores">
+				<li data-target="#temas" data-slide-to="0" class="active marcadores"></li>
+				<li data-target="#temas" data-slide-to="1" class="marcadores"></li>
+			</ol>
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+
+					<div class="row">
+
+						<div class="col-md-12 col-lg-10 col-lg-offset-1">
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h3 class="panel-title text-center">
+										Fundamentos de Probabilidad
+									</h3>
+								</div>
+								<div class="panel-body espaciado_panel text-justify">
+
+									<strong>Introducción a la Teoría de la probabilidad:</strong> Experimento aleatorio, evento y espacio muestral. Definición de probabilidad. Propiedades de la probabilidad de un suceso. Ley aditiva de probabilidad. Probabilidad condicional. Ley multiplicativa. Dependencia e independencia de sucesos. Teorema de Bayes.
+									
+									<a href="#">
+										Ver detalles
+									</a>
+
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="row">
+
+						<div class="col-md-12 espacio_blanco">
+
+						</div>
+
+					</div>
+
+					<div class="row">
+
+						<div class="col-md-6 col-lg-5 col-lg-offset-1">
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h3 class="panel-title text-center">
+										Variables aleatorias
+									</h3>
+								</div>
+								<div class="panel-body espaciado_panel text-justify">
+									
+									Definición de variables aleatorias. Variables aleatorias discretas y continuas. Distribución de probabilidad. Esperanza y Varianza de una variable aleatoria. Función generadora de momentos. Transformación de una variable aleatoria.
+
+									<a href="#">
+										Ver detalles
+									</a>
+
+								</div>
+							</div>
+
+						</div>
+
+						<div class="col-md-6 col-lg-5">
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h3 class="panel-title text-center">
+										Distribuciones
+									</h3>
+								</div>
+								<div class="panel-body espaciado_panel text-justify">
+
+									Distribuciones con nombre propio. Distribuciones discretas: Bernoulli, Geométrica, Binomial, Binomial negativa, Hipergeométrica, Uniforme y Poisson. Distribuciones Continuas especiales: Uniforme, Exponencial, Gamma, Normal.
+
+									<a href="#">
+										Ver detalles
+									</a>
+
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+					
+				</div>
+				<div class="item">
+
+					<div class="row">
+
+						<div class="col-md-12 col-lg-10 col-lg-offset-1">
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h3 class="panel-title text-center">
+										Variables aleatorias: caso bidimensional
+									</h3>
+								</div>
+								<div class="panel-body text-justify">
+
+									Distribución de probabilidad Conjunta. Funciones de probabilidad y de densidad conjunta. Función de probabilidad y de densidad marginal. Probabilidad condicional. Esperanza y Varianza. Dependencia e Independencia de variables aleatorias, Covarianza y correlación.
+
+									<a href="#">
+										Ver detalles
+									</a>
+
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="row">
+
+						<div class="col-md-12 col-lg-10 col-lg-offset-1">
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h3 class="panel-title text-center">
+										Teorema Central del Límite e Intervalos de Confianza
+									</h3>
+								</div>
+								<div class="panel-body cuerpo_panel text-justify">
+
+									Introducción a las muestras aleatorias y distribuciones de muestreo de estadísticas (media y varianza muestral). Estadística Descriptiva. Intervalos para la media. 
+
+									<a href="#">
+										Ver detalles
+									</a>
+
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="row">
+
+						<div class="col-md-6 col-lg-5 col-lg-offset-1">
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h3 class="panel-title text-center">
+										Prueba de Hipótesis
+									</h3>
+								</div>
+								<div class="panel-body">
+
+									Errores tipo I y II. Potencia de la Prueba. Significancia.
+
+									<a href="#">
+										Ver detalles
+									</a>
+
+								</div>
+							</div>
+
+						</div>
+
+						<div class="col-md-6 col-lg-5">
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h3 class="panel-title text-center">
+										Cadenas de Markov
+									</h3>
+								</div>
+								<div class="panel-body">
+
+									Clasificación de estados, vector de probabilidades límite.
+
+									<a href="#">
+										Ver detalles
+									</a>
+
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+					
+				</div>
+
+			</div>
+
+			<!-- Controls -->
+			<a class="left carousel-control carrusel_sin_sombra" href="#temas" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left flechas" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control carrusel_sin_sombra" href="#temas" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right flechas" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+
+		<!--Finaliza carrusel-->
+
+	</div>
+
+</div>
---
+++ resources/views/public/proba/index_components/galeria_xs_sm.blade.php
@@ -0,0 +1,158 @@
+<div class="row hidden-md hidden-lg">
+
+  <div class="col-xs-12 col-sm-12">
+
+    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+      <div class="panel panel-primary">
+        <div class="panel-heading" role="tab" id="fundamentos_header">
+          <h4 class="panel-title">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#fundamentos" aria-expanded="true" aria-controls="fundamentos">
+              Fundamentos de Probabilidad
+            </a>
+          </h4>
+        </div>
+        <div id="fundamentos" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="fundamentos_header">
+          <div class="panel-body">
+
+            <strong>Introducción a la Teoría de la probabilidad:</strong> Experimento aleatorio, evento y espacio muestral. Definición de probabilidad. Propiedades de la probabilidad de un suceso. Ley aditiva de probabilidad. Probabilidad condicional. Ley multiplicativa. Dependencia e independencia de sucesos. Teorema de Bayes.
+                  
+            <a href="#">
+              Ver detalles
+            </a>
+
+          </div>
+        </div>
+      </div>
+    
+      <div class="panel panel-primary">
+        <div class="panel-heading" role="tab" id="variables_header">
+          <h4 class="panel-title">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#variables" aria-expanded="true" aria-controls="variables">
+              Variables aleatorias
+            </a>
+          </h4>
+        </div>
+        <div id="variables" class="panel-collapse collapse" role="tabpanel" aria-labelledby="variables_header">
+          <div class="panel-body">
+          
+            Definición de variables aleatorias. Variables aleatorias discretas y continuas. Distribución de probabilidad. Esperanza y Varianza de una variable aleatoria. Función generadora de momentos. Transformación de una variable aleatoria.
+                  
+            <a href="#">
+              Ver detalles
+            </a>
+
+          </div>
+        </div>
+      </div>
+    
+      <div class="panel panel-primary">
+        <div class="panel-heading" role="tab" id="distribuciones_header">
+          <h4 class="panel-title">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#distribuciones" aria-expanded="true" aria-controls="distribuciones">
+              Distribuciones
+            </a>
+          </h4>
+        </div>
+        <div id="distribuciones" class="panel-collapse collapse" role="tabpanel" aria-labelledby="distribuciones_header">
+          <div class="panel-body">
+          
+            Distribuciones con nombre propio. Distribuciones discretas: Bernoulli, Geométrica, Binomial, Binomial negativa, Hipergeométrica, Uniforme y Poisson. Distribuciones Continuas especiales: Uniforme, Exponencial, Gamma, Normal.
+                  
+            <a href="#">
+              Ver detalles
+            </a>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="panel panel-primary">
+        <div class="panel-heading" role="tab" id="bidimensional_header">
+          <h4 class="panel-title">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#bidimensional" aria-expanded="true" aria-controls="bidimensional">
+              Variables aleatorias: caso bidimensional
+            </a>
+          </h4>
+        </div>
+        <div id="bidimensional" class="panel-collapse collapse" role="tabpanel" aria-labelledby="bidimensional_header">
+          <div class="panel-body">
+          
+            Distribución de probabilidad Conjunta. Funciones de probabilidad y de densidad conjunta. Función de probabilidad y de densidad marginal. Probabilidad condicional. Esperanza y Varianza. Dependencia e Independencia de variables aleatorias, Covarianza y correlación.
+                  
+            <a href="#">
+              Ver detalles
+            </a>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="panel panel-primary">
+        <div class="panel-heading" role="tab" id="tcl_header">
+          <h4 class="panel-title">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#tcl" aria-expanded="true" aria-controls="tcl">
+              Teorema Central del Límite e Intervalos de Confianza
+            </a>
+          </h4>
+        </div>
+        <div id="tcl" class="panel-collapse collapse" role="tabpanel" aria-labelledby="tcl_header">
+          <div class="panel-body">
+          
+            Introducción a las muestras aleatorias y distribuciones de muestreo de estadísticas (media y varianza muestral). Estadística Descriptiva. Intervalos para la media. 
+                  
+            <a href="#">
+              Ver detalles
+            </a>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="panel panel-primary">
+        <div class="panel-heading" role="tab" id="test_header">
+          <h4 class="panel-title">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#test" aria-expanded="true" aria-controls="test">
+              Prueba de Hipótesis
+            </a>
+          </h4>
+        </div>
+        <div id="test" class="panel-collapse collapse" role="tabpanel" aria-labelledby="test_header">
+          <div class="panel-body">
+          
+            Errores tipo I y II. Potencia de la Prueba. Significancia. 
+                  
+            <a href="#">
+              Ver detalles
+            </a>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="panel panel-primary">
+        <div class="panel-heading" role="tab" id="markov_header">
+          <h4 class="panel-title">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#markov" aria-expanded="true" aria-controls="markov">
+              Cadenas de Markov
+            </a>
+          </h4>
+        </div>
+        <div id="markov" class="panel-collapse collapse" role="tabpanel" aria-labelledby="markov_header">
+          <div class="panel-body">
+          
+            Clasificación de estados, vector de probabilidades límite.
+                  
+            <a href="#">
+              Ver detalles
+            </a>
+
+          </div>
+        </div>
+      </div>
+    
+    </div>
+
+  </div>
+
+</div>
